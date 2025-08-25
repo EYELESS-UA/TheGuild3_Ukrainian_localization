e0995fc72c3b07b50b6381c922c02c2668b71771
Євген <118132275+EYELESS-UA@users.noreply.github.com>
2025-08-26 00:41:25 +0300

    start

---
+++ G3Ua_Steam/publish_mod.bat
@@ -0,0 +1,1 @@
+Guild3ModUploader.exe publish --title "Modding" --description "The Guild 3 Mod" --content ".\content" --preview ".\preview.png"
---
+++ G3Ua_Steam/update_mod.bat
@@ -0,0 +1,1 @@
+Guild3ModUploader.exe update --mod 3507960492 --content .\content --preview .\preview.png
